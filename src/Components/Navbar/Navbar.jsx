a45b47f25aa0fa20d621b3f071248d40f9a699d1
Hoshhh <nyxious123@live.com>
2022-05-12 22:11:11 -0400

    more navbar work

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import '../Navbar/navbar.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 const Navbar = () => {
   return (
     <div className='nav-container'>
         <span className='nav-logo'>VisuaLink</span>
-        <form action="">
-            <input className='nav-input' type="text" />
-        </form>
+        <div className='search-container'>
+            <SearchIcon className='nav-search-icon'/>
+            <form action="">
+                <input className='nav-input' type="text" />
+            </form>
+        </div>
         <div className='nav-buttons'>
             <button className='btn'>Signup</button>
             <button className='btn'>Login</button>
